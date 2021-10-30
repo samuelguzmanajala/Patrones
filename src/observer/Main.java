cc4fd3556a3de00ab02f1988880534b6a9b98798
samuel Guzman <samuelguzman@pop-os.localdomain>
2021-10-30 08:18:06 +0200

    Observer

--- src/observer/Main.java
+++ src/observer/Main.java
@@ -2,14 +2,14 @@ package observer;
 
 public class Main {
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
 //		Observable pacient=new Covid19Pacient("aitor", 35);
 //		new PacientObserverGUI (pacient);
 //		new PacientSymptomGUI (pacient);
-	}
+    }
 
 
 }
